b7e26385dc0cb1c58bf4fd56038e5d10e10b03d1
Camilo Orozco Hdz <camilo.orozcohdz@gmail.com>
2026-07-04 20:56:37 +0200

    Se configura y se crea el provider del queryClient para el uso de React Query

--- src/app/providers/AppProvider.tsx
+++ src/app/providers/AppProvider.tsx
@@ -1,5 +1,11 @@
 import { AppRouter } from '@/app/router/AppRouter';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './query/queryClient';
 
 export const AppProviders = () => {
-  return <AppRouter />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+    </QueryClientProvider>
+  );
 };
---
+++ src/shared/query/queryKeys.ts
@@ -0,0 +1,7 @@
+export const queryKeys = {
+  boards: ['boards'] as const,
+
+  lists: ['lists'] as const,
+
+  tasks: ['tasks'] as const,
+};
