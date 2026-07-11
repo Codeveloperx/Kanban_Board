75c1fe40e149332a4ad03952892d484d88bfd6a5
Camilo Orozco Hdz <camilo.orozcohdz@gmail.com>
2026-07-11 20:07:28 +0200

    se hacen ajustes en el còdigo

--- src/shared/storage/localStorage.ts
+++ src/shared/storage/localStorage.ts
@@ -5,23 +5,33 @@ import type { Storage } from './storage';
 export class LocalStorage implements Storage {
   async load<T>(): Promise<T | null> {
     try {
-      const values = localStorage.getItem(STORAGE_KEY);
-      return values ? (JSON.parse(values) as T) : null;
+      const data = localStorage.getItem(STORAGE_KEY);
+
+      return data ? (JSON.parse(data) as T) : null;
     } catch (error) {
       console.error('Error loading data from localStorage:', error);
+
+      localStorage.removeItem(STORAGE_KEY);
+
       return null;
     }
   }
 
   async save<T>(data: T): Promise<void> {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+    try {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+    } catch (error) {
+      console.error('Error saving data to localStorage:', error);
+      throw error;
+    }
   }
 
   async remove(): Promise<void> {
-    localStorage.removeItem(STORAGE_KEY);
-  }
-
-  async clear(): Promise<void> {
-    localStorage.clear();
+    try {
+      localStorage.removeItem(STORAGE_KEY);
+    } catch (error) {
+      console.error('Error removing data from localStorage:', error);
+      throw error;
+    }
   }
 }
--- src/shared/storage/storage.ts
+++ src/shared/storage/storage.ts
@@ -2,5 +2,4 @@ export interface Storage {
   load<T>(): Promise<T | null>;
   save<T>(data: T): Promise<void>;
   remove(): Promise<void>;
-  clear(): Promise<void>;
 }
