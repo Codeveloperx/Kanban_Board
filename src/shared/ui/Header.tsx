ceccf24ed08a81b1a3f79d68e4031e3188cc0bee
Camilo Orozco Hdz <camilo.orozcohdz@gmail.com>
2026-07-12 19:19:03 +0200

    Ajustes en el componente header

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -1,12 +1,13 @@
 type Propstype = {
   children?: React.ReactNode;
+  title?: string;
 };
 
-export const Header = ({ children }: Propstype) => {
+export const Header = ({ children, title = 'Kanban Board' }: Propstype) => {
   return (
     <header className="bg-gray-800 text-white p-4 w-full">
       <div className="flex items-center justify-between">
-        <span className="text-lg">Kanban Board</span>
+        <span className="text-lg">{title}</span>
         <div>{children}</div>
       </div>
     </header>
